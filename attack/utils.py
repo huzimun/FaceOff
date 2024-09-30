6381b80624a3754c53f542232f9142df1b60390d
huzimun <humingwang@stu.ouc.edu.cn>
2024-09-30 09:43:13 +0800

    add utils

---
+++ attack/utils.py
@@ -0,0 +1,7 @@
+import json
+from typing import Any, Mapping
+
+def read_json(filename: str) -> Mapping[str, Any]:
+    """Returns a Python dict representation of JSON object at input file."""
+    with open(filename) as fp:
+        return json.load(fp)

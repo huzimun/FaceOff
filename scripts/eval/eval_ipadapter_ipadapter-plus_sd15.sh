563d9f3e644d7be08fdb3f37859b52034c7ea469
huzimun <humingwang@stu.ouc.edu.cn>
2025-12-20 21:20:01 +0800

    add ipadapter and ipadapter plus with sd15

---
+++ scripts/eval/eval_ipadapter_ipadapter-plus_sd15.sh
@@ -0,0 +1,54 @@
+export adversarial_folder_name="ipadapter,ipadapter-plus,photomaker_VGGFace2_gau,none_kernel7_cosine_w0.5_num100_alpha0.005_eps16_input224_yingbu"
+export customization_folder_name="ipadapter_sd1-5_${adversarial_folder_name}"
+export customization_output_dir="./outputs/customization_outputs/${customization_folder_name}"
+export evaluation_output_dir="./outputs/evaluation_outputs/${customization_folder_name}"
+export prompts="a_photo_of_person;a_dslr_portrait_of_person"
+export dataset="./outputs/adversarial_images/VGGFace2"
+export map_json_path="./max_clip_cosine_distance_map_VGGFace2.json"
+echo $prompts
+
+export save_config_dir="./outputs/config_scripts_logs/${adversarial_folder_name}"
+mkdir $save_config_dir
+cp "./scripts/eval/eval_ipadapter_ipadapter-plus_sd15.sh" $save_config_dir
+
+# original output and original input
+# ArcFace
+python ./evaluations/ism_fdfr.py \
+    --prompts $prompts \
+    --data_dir $customization_output_dir \
+    --emb_dirs $dataset \
+    --save_dir $evaluation_output_dir \
+    --scene "protected_output" \
+    --scene2 "original_input" \
+    --is_target 0 \
+    --map_path $map_json_path \
+    --target_path "" \
+    --model_name "ArcFace" \
+    --input_name "set_B" \
+    --out_out 0
+
+export customization_folder_name="ipadapter-plus_sd1-5_${adversarial_folder_name}"
+export customization_output_dir="./outputs/customization_outputs/${customization_folder_name}"
+export evaluation_output_dir="./outputs/evaluation_outputs/${customization_folder_name}"
+export prompts="a_photo_of_person;a_dslr_portrait_of_person"
+echo $prompts
+
+export save_config_dir="./outputs/config_scripts_logs/${adversarial_folder_name}"
+mkdir $save_config_dir
+cp "./scripts/eval/eval_ipadapter_ipadapter-plus_sd15.sh" $save_config_dir
+
+# original output and original input
+# ArcFace
+python ./evaluations/ism_fdfr.py \
+    --prompts $prompts \
+    --data_dir $customization_output_dir \
+    --emb_dirs $dataset \
+    --save_dir $evaluation_output_dir \
+    --scene "protected_output" \
+    --scene2 "original_input" \
+    --is_target 0 \
+    --map_path $map_json_path \
+    --target_path "" \
+    --model_name "ArcFace" \
+    --input_name "set_B" \
+    --out_out 0
---
+++ scripts/gen/gen_ipadapter_ipadapter-plus_sd15.sh
@@ -0,0 +1,35 @@
+export adversarial_folder_name="ipadapter,ipadapter-plus,photomaker_VGGFace2_gau,none_kernel7_cosine_w0.5_num100_alpha0.005_eps16_input224_yingbu"
+export save_config_dir="./outputs/config_scripts_logs/${adversarial_folder_name}"
+mkdir $save_config_dir
+cp "./scripts/gen/gen_ipadapter_ipadapter-plus_sd15.sh" $save_config_dir
+export device="cuda:5"
+
+# base model is SD 1.5
+export experiment_name="ipadapter_sd1-5_"$adversarial_folder_name
+python ./customization/target_model/IP-Adapter/a_ip_adapter_sdxl_demo.py \
+    --model_type "sd15" \
+    --base_model_path "/home/humw/Pretrains/stable-diffusion-v1-5" \
+    --image_encoder_path "/home/humw/Pretrains/h94/IP-Adapter/models/image_encoder" \
+    --ip_ckpt "/home/humw/Pretrains/h94/IP-Adapter/models/ip-adapter_sd15.bin" \
+    --vae_model_path "/home/humw/Pretrains/sd-vae-ft-mse" \
+    --device $device \
+    --input_dir "./outputs/adversarial_images/${adversarial_folder_name}" \
+    --output_dir "./outputs/customization_outputs/${experiment_name}" \
+    --resolution 224 \
+    --sub_name "" \
+    --prior_generation_precision "fp16"
+
+# base model is SD 1.5
+export experiment_name="ipadapter-plus_sd1-5_"$adversarial_folder_name
+python ./customization/target_model/IP-Adapter/a_ip_adapter_sdxl_plus-face_demo.py \
+    --model_type "sd15" \
+    --base_model_path "/home/humw/Pretrains/stable-diffusion-v1-5" \
+    --image_encoder_path "/home/humw/Pretrains/h94/IP-Adapter/models/image_encoder" \
+    --ip_ckpt "/home/humw/Pretrains/h94/IP-Adapter/models/ip-adapter-plus_sd15.bin" \
+    --vae_model_path "/home/humw/Pretrains/sd-vae-ft-mse" \
+    --device $device \
+    --input_dir "./outputs/adversarial_images/${adversarial_folder_name}" \
+    --output_dir "./outputs/customization_outputs/${experiment_name}" \
+    --resolution 224 \
+    --sub_name "" \
+    --prior_generation_precision "fp16"
